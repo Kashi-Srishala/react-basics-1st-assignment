ebb262c310f99550deaba743327b4cbf4b258d88
Kashi-Srishala <kashisrishala@gmail.com>
2023-05-01 17:27:49 +0530

    Added Change Expenses Button to $100

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,28 @@
+import React, {useState} from 'react';
 import ExpenseDate  from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) =>{
+    const [title, setTitle] = useState(props.title);
+    const [amount, setAmount] = useState(props.amount);
+    
     const clickHandler = () => {
-        console.log('Clicked!!!')
+        setTitle('Updated!');
+        console.log(title);
+    }
+    const clickHandler2 = () => {
+        setAmount(100);
+        console.log(amount);
     }
     return (
         <Card className="expense-item">
 
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">${amount}</div>
+                <button onClick={clickHandler2}>Change Expense</button>
             </div>
             <button onClick={clickHandler}>Delete Expense</button>          
         </Card>      
