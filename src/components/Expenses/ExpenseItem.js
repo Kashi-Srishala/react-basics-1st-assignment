4e20f1574a0ed87942c85cf169f92970490fd656
Kashi-Srishala <kashisrishala@gmail.com>
2023-05-07 21:27:14 +0530

    Added filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,19 @@
-import React, {useState} from 'react';
-import ExpenseDate  from './ExpenseDate';
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) =>{
-    const [title, setTitle] = useState(props.title);
-    const [amount, setAmount] = useState(props.amount);
-    
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    }
-    const clickHandler2 = () => {
-        setAmount(100);
-        console.log(amount);
-    }
-    return (
-        <Card className="expense-item">
-
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${amount}</div>
-                <button onClick={clickHandler2}>Change Expense</button>
-            </div>
-            <button onClick={clickHandler}>Delete Expense</button>          
-        </Card>      
-    );   
+const ExpenseItem = (props) => {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
+
 export default ExpenseItem;
